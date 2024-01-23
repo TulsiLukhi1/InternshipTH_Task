edad7b0f60547d693a79b32d4e56e7f3cce2eb37
TulsiLukhi1 <lukhitulshi20137@gmail.com>
2024-01-24 00:35:03 +0530

    Add ChunkArray task

---
+++ ChunkArray/chunkarray.js
@@ -0,0 +1,27 @@
+var chunk = function (arr, size) {
+  let start = 0;
+  let ans = [];
+  for (let i = 0; i < Math.ceil(arr.length / size); i++) {
+    if (start + size > arr.length) {
+      ans = [...ans, arr.slice(start, arr.length)];
+    } else {
+      ans = [...ans, arr.slice(start, size + start)];
+    }
+
+    start = start + size;
+  }
+  return ans;
+};
+
+
+
+
+const test = ()=>{
+    const arr = [1,2,3,4,5,6,7,8,9];
+    const size=2;
+    const chunkArray = chunk(arr,size);
+    console.log("result",chunkArray);
+   
+}
+
+test();
