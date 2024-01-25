157b4971eb984ead772f4f4eb40e150698c56f11
TulsiLukhi1 <lukhitulshi20137@gmail.com>
2024-01-25 17:22:45 +0530

    added ternary operator ,changed == to ===

--- ChunkArray/chunkarray.js
+++ ChunkArray/chunkarray.js
@@ -2,30 +2,25 @@ var chunk = function (arr, size) {
   let start = 0;
   let ans = [];
 
-  if(size == 0){
+  if (size === 0) {
     return arr;
   }
   for (let i = 0; i < Math.ceil(arr.length / size); i++) {
-    if (start + size > arr.length) {
-      ans = [...ans, arr.slice(start, arr.length)];
-    } else {
-      ans = [...ans, arr.slice(start, size + start)];
-    }
+    
+    start + size > arr.length
+      ? (ans = [...ans, arr.slice(start, arr.length)])
+      : (ans = [...ans, arr.slice(start, size + start)]);
 
     start = start + size;
   }
   return ans;
 };
 
-
-
-
-const test = ()=>{
-    const arr = [1,2,3,4,5,6,7,8,9];
-    const size=2;
-    const chunkArray = chunk(arr,size);
-    console.log("result",chunkArray);
-   
-}
+const test = () => {
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const size = 2;
+  const chunkArray = chunk(arr, size);
+  console.log("result", chunkArray);
+};
 
 test();
