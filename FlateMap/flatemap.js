9ebaf10b728e06321752beb6d509483f1b0be394
TulsiLukhi1 <lukhitulshi20137@gmail.com>
2024-01-31 10:01:20 +0530

    Flaten array with recursion using reduce and concat

--- FlateMap/flatemap.js
+++ FlateMap/flatemap.js
@@ -1,13 +1,17 @@
-
-
-
-    
-const addArray = (...args)=>{
-    const singleArr = args.flat(Infinity);
-    return singleArr;
-}
+const flattenTheArray = (...arrays) => {
+    return arrays.reduce(
+      (accumulator, currentValue) =>
+        Array.isArray(currentValue)
+          ? accumulator.concat(flattenTheArray(...currentValue))
+          : accumulator.concat(currentValue),
+      []
+    );
+  };
   
-
-  
-addArray([1,2,3,[0,2]],[4,5,[11,12,[30,40,[80]]]],[9],[6,7,8,9,10,11]);
+  console.log(flattenTheArray(
+    [1, 2, 3, [0, 2]],
+    [4, 5, [11, 12, [30, 40, [80]]]],
+    [9],
+    [6, 7, 8, 9, 10, 11]
+  ));
   
